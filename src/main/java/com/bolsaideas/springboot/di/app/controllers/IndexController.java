926423eba04af68a2b747395bf0133f93421cdd2
Adrián Antón <adrtler@gmail.com>
2022-02-15 12:27:19 +0100

    Inyectando mediante la interfaz

--- src/main/java/com/bolsaideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/di/app/controllers/IndexController.java
@@ -1,20 +1,24 @@
 package com.bolsaideas.springboot.di.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.bolsaideas.springboot.di.app.models.service.MiServicio;
+import com.bolsaideas.springboot.di.app.models.service.IServicio;
 
 @Controller
 public class IndexController {
 
 	/**
 	 * Con autowired spring maneja las inyecciones de dependencias automáticamente
+	 * Con el qualifier indicamos que componente de los que implementan la interfaz
+	 * queremos implementar
 	 */
 	@Autowired
-	private MiServicio servicio;
+	@Qualifier(value = "miServicioSimple")
+	private IServicio servicio;
 
 	@GetMapping({ "/index", "/", "home" })
 	public String index(Model model) {
--- src/main/java/com/bolsaideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsaideas/springboot/di/app/models/service/MiServicio.java
@@ -2,8 +2,17 @@ package com.bolsaideas.springboot.di.app.models.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class MiServicio {
+/**
+ * Le damos un nombre al servicio para diferenciar entre las diferentes
+ * implementaciones de la interfaz IServicio
+ * 
+ * @author Adrián
+ *
+ */
+@Service("miServicioSimple")
+public class MiServicio implements IServicio {
+
+	@Override
 	public String operacion() {
 		return "Ejecutando algún proceso importante...";
 	}
