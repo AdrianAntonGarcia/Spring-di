6393becff9be51197aee3298af3c1b84b881d70f
Adrián Antón <adrtler@gmail.com>
2022-02-15 17:47:49 +0100

    Registrando el componente ItemsFactura

--- src/main/java/com/bolsaideas/springboot/di/app/AppConfig.java
+++ src/main/java/com/bolsaideas/springboot/di/app/AppConfig.java
@@ -1,9 +1,14 @@
 package com.bolsaideas.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.bolsaideas.springboot.di.app.models.domain.ItemFactura;
+import com.bolsaideas.springboot.di.app.models.domain.Producto;
 import com.bolsaideas.springboot.di.app.models.service.IServicio;
 import com.bolsaideas.springboot.di.app.models.service.MiServicio;
 import com.bolsaideas.springboot.di.app.models.service.MiServicioComplejo;
@@ -20,5 +25,16 @@ public class AppConfig {
 	@Primary
 	public IServicio registrarMiServicioComplejo() {
 		return new MiServicioComplejo();
+	}
+
+	@Bean("itemsFactura")
+	public List<ItemFactura> registrarItems() {
+		Producto producto1 = new Producto("Camara Sony", 100);
+		Producto producto2 = new Producto("Bicicleta Bianchi aro 26", 200);
+
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 3);
+
+		return Arrays.asList(linea1, linea2);
 	}
 }
